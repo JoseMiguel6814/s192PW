42ea4f18674ec8f3c0cdcb89bf7eeb248012de41
JoseMiguel6814 <jowsjose6814@gmail.com>
2024-09-24 18:56:51 -0600

    Avance practica 3

--- introlaravel/routes/web.php
+++ introlaravel/routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+/* 
 Route::get('/', function () {
     return view('welcome');
 });
+ */
+
+ //ruta de tipo view 
+Route::view('/','welcome');
+Route::view('/form','formulario');
