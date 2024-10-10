53eaf0cbdefb144e1dd5190e4860cd152f933e94
JoseMiguel6814 <jowsjose6814@gmail.com>
2024-10-09 18:58:17 -0600

    AVANCES ACTIVIDAD 4 CONTROLADORES

--- introlaravel/routes/web.php
+++ introlaravel/routes/web.php
@@ -1,21 +1,56 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\controladorVistas;
 
-//Shift alt a
-//ruta tipo get
-/* Route::get('/', function () {
-    return view('welcome');
-}); */
+
+Route::get('/',[controladorVistas::class, 'home'])->name('inicio');
+Route::get('/form',[controladorVistas::class, 'formulario'])->name('formulario');
+Route::get('/clientes',[controladorVistas::class, 'consulta'])->name('clientes');
+
+Route::view('/comp', 'componentes')->name('componentes');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 //ruta tipo view
-Route::view('/','inicio' );
+/* Route::view('/','inicio' );
 
 Route::view('/form', 'formulario')->name('formulario');
 
 Route::view('/clientes', 'clientes')->name('clientes');
 
 Route::view('/inicio', 'inicio')->name('inicio');
+ */
 
-Route::view('/comp', 'componentes')->name('componentes');
